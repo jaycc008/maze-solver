e88f90557aef02447b3f1c191d3c6614fafad1bf
jayce008 <jayce.ardon@gmail.com>
2025-03-24 17:16:15 +0100

    break walls recursively

--- maze.py
+++ maze.py
@@ -1,3 +1,4 @@
+import random
 import time
 from cell import Cell
 
@@ -12,6 +13,7 @@ class Maze:
         cell_size_x,
         cell_size_y,
         win = None,
+        seed = None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -22,6 +24,9 @@ class Maze:
         self._win = win
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0, 0)
+        if seed is not None:
+            random.seed(seed)
     
     def _create_cells(self):
         self._cells = []
@@ -46,10 +51,54 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.001)
+        time.sleep(0.05)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
         self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
         self._draw_cell(0, 0)
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+
+        while True:
+            possible_directions = []
+            # left
+            if i > 0 and not self._cells[i - 1][j].visited:
+                possible_directions.append((i - 1, j))
+            # right
+            if i < self._num_cols - 1 and not self._cells[i + 1][j].visited:
+                possible_directions.append((i + 1, j))
+            # up
+            if j > 0 and not self._cells[i][j - 1].visited:
+                possible_directions.append((i, j - 1))
+            # down
+            if j < self._num_rows - 1 and not self._cells[i][j + 1].visited:
+                possible_directions.append((i, j + 1))
+
+            if len(possible_directions) == 0:
+                self._draw_cell(i, j)
+                return
+            
+            dir_index = random.randrange(len(possible_directions))
+            direction = possible_directions[dir_index]
+            
+            # left
+            if direction[0] == i - 1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i-1][j].has_right_wall = False
+            # right
+            if direction[0] == i + 1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i+1][j].has_left_wall = False
+            # up
+            if direction[1] == j - 1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j-1].has_bottom_wall = False
+            # down
+            if direction[1] == j + 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j+1].has_top_wall = False
+            
+            self._break_walls_r(direction[0], direction[1])
